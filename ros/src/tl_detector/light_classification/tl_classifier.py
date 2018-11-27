ce06078f9c23eda472228e3d9ec8b9e795d0b2f5
Frank Knoll <Knoll_Frank@web.de>
2018-11-27 23:12:59 +0100

    calling prediction

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,44 @@
+import rospy
 from styx_msgs.msg import TrafficLight
+from keras import __version__ as keras_version
+from keras.models import load_model
+import h5py
+import cv2
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+
+factor = 1.0 / 4.0
+
+def resize(image):
+    return cv2.resize(image, dsize=None, fx=factor, fy=factor)
+
+
+def preprocess(image):
+    return resize(image)
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        # TODO load classifier
+        self.model = load_model('light_classification/model_lenet.h5')
+        # see: https://stackoverflow.com/questions/47115946/tensor-is-not-an-element-of-this-graph
+        self.graph = tf.get_default_graph()
+        self.encoder = LabelEncoder()
+        self.encoder.classes_ = np.load('light_classification/classes.npy')
+
+    def asTrafficLightState(self, stateStr):
+        if stateStr == "red":
+            return TrafficLight.RED
+        if stateStr == "yellow":
+            return TrafficLight.YELLOW
+        if stateStr == "green":
+            return TrafficLight.GREEN
+        if stateStr == "unknown":
+            return TrafficLight.UNKNOWN
+        if stateStr == "no_traffic_light":
+            return TrafficLight.UNKNOWN
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +50,10 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        # TODO implement light color prediction
+        image_array = preprocess(np.asarray(image))
+        with self.graph.as_default():
+            ynew = self.model.predict(image_array[None, :, :, :])
+        label = self.encoder.inverse_transform([np.argmax(ynew)])[0]
+        rospy.loginfo("label: %s", label)
+        return self.asTrafficLightState(label)
