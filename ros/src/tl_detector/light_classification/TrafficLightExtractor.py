59f3dadf15f752d40d5f28c7c8c9f117b5513385
Frank Knoll <Knoll_Frank@web.de>
2018-12-05 01:13:37 +0100

    refactoring

--- ros/src/tl_detector/light_classification/TrafficLightExtractor.py
+++ ros/src/tl_detector/light_classification/TrafficLightExtractor.py
@@ -62,10 +62,8 @@ class TrafficLightExtractor:
                         tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
                             tensor_name)
                 if 'detection_masks' in tensor_dict:
-                    # The following processing is only for single image
                     detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
                     detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-                    # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
                     real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
                     detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
                     detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
@@ -73,16 +71,13 @@ class TrafficLightExtractor:
                         detection_masks, detection_boxes, image.shape[0], image.shape[1])
                     detection_masks_reframed = tf.cast(
                         tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-                    # Follow the convention by adding back the batch dimension
                     tensor_dict['detection_masks'] = tf.expand_dims(
                         detection_masks_reframed, 0)
                 image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
 
-                # Run inference
                 output_dict = sess.run(tensor_dict,
                                        feed_dict={image_tensor: np.expand_dims(image, 0)})
 
-                # all outputs are float32 numpy arrays, so convert types as appropriate
                 output_dict['num_detections'] = int(output_dict['num_detections'][0])
                 output_dict['detection_classes'] = output_dict[
                     'detection_classes'][0].astype(np.uint8)
@@ -92,15 +87,11 @@ class TrafficLightExtractor:
                     output_dict['detection_masks'] = output_dict['detection_masks'][0]
         return output_dict
 
-    def bla(self, TEST_IMAGE_PATHS, dst):
-        for i, image_path in enumerate(TEST_IMAGE_PATHS):
+    def bla(self, imagePaths, dst):
+        for i, image_path in enumerate(imagePaths):
             image = Image.open(image_path)
             image_np = self.load_image_into_numpy_array(image)
-            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-            image_np_expanded = np.expand_dims(image_np, axis=0)
-            # Actual detection.
             output_dict = self.run_inference_for_single_image(image_np)
-            result = Image.fromarray(image_np)
             (im_width, im_height) = image.size
             for j, box in enumerate(output_dict['detection_boxes']):
                 if output_dict['detection_classes'][j] == 10:
@@ -111,5 +102,5 @@ class TrafficLightExtractor:
 
     def extractTrafficLights(self, srcDir, dstDir):
         mkdir(dstDir + '/green')
-        TEST_IMAGE_PATHS = glob.glob(srcDir + "/green/*")
-        self.bla(TEST_IMAGE_PATHS, dstDir + '/green')
+        imagePaths = glob.glob(srcDir + "/green/*")
+        self.bla(imagePaths, dstDir + '/green')
